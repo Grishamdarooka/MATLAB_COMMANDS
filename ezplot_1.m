c61d85497607b848beb09b1500b22b751501ea13
Grishamdarooka <163041883+Grishamdarooka@users.noreply.github.com>
2025-03-02 15:43:28 +0530

    Delete ezplot_1.m

--- ezplot_1.m
+++
@@ -1,5 +0,0 @@
-clear all; close all; clc
-ezplot(@(x,y)(x.^2 + y.^2).^2 - x.^2 + y.^2,[-1.1 1.1 -1.1 1.1])
-% the values in [] are the axis limit
-% @ is used to define the variables 
-% after defining the equation in () is the function we are working on
